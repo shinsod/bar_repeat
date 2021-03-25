d583249321e9a66e4343aa6663c9c9c4d3c947f5
ekselno <ekselno@mail.ru>
2021-03-25 22:59:44 +0300

    promo_right

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.carousel-promo').slick({
+        prevArrow: '<button type="button" class="slick-prev"> <img src="icons/arrows/arrow-prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"> <img src="icons/arrows/arrow-next.png"></button>',
+        dots: true
+    });
+  });
