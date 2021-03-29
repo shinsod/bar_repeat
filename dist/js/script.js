eb5f9982fde402b2fd65316c942a3d147139976f
ekselno <ekselno@mail.ru>
2021-03-29 17:25:53 +0300

    promo media

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,6 +2,21 @@ $(document).ready(function(){
     $('.carousel-promo').slick({
         prevArrow: '<button type="button" class="slick-prev"> <img src="icons/arrows/arrow-prev.png"></button>',
         nextArrow: '<button type="button" class="slick-next"> <img src="icons/arrows/arrow-next.png"></button>',
-        dots: true
+        appendDots:$('.promo__counter'),
+        dots: true,
+        responsive:[
+            {
+                breakpoint: 1760,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
+  });
+
+  $('.carousel-promo').on('afterChange', function(event, slick, currentSlide, nextSlide){
+    let slide_num = currentSlide+1;
+    if (slide_num < 10) { slide_num = '0' + slide_num; }
+    $('.promo__counter-min').text(slide_num);
   });
