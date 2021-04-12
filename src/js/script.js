69963023a17feee088a89c195423c16568e85ab8
ekselno <ekselno@mail.ru>
2021-04-12 21:17:11 +0300

    pop-up

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,9 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"> <img src="icons/arrows/arrow-next.png"></button>',
         appendDots:$('.promo__counter'),
         dots: true,
+        arrows: true,
         responsive:[
+            
             {
                 breakpoint: 1760,
                 settings: {
@@ -20,3 +22,34 @@ $(document).ready(function(){
     if (slide_num < 10) { slide_num = '0' + slide_num; }
     $('.promo__counter-min').text(slide_num);
   });
+
+    /*  menu-mobile */
+
+    const humburger = document.querySelector('.header__humburger'),
+            menu = document.querySelector('.mobile-menu'),
+            closerf = document.querySelector('.mobile-menu__close');
+
+    humburger.addEventListener('click', () => {
+        menu.classList.add('active');
+    });
+
+    closerf.addEventListener('click', () => {
+        menu.classList.remove('active');
+
+    //modal
+
+ 
+
+});
+
+$('[data-modal=cons]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+});
+
+$('.modal__close').on('click', function(){
+    $('#thanks, .overlay, #consultation').fadeOut('slow');
+});
+   
+
+
+
